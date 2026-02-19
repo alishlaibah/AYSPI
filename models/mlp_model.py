92f2b09f14fee6f0b2b09c5b64f13180e0067170
Ali Shlaibah <shlaibaha@gmail.com>
2026-02-19 10:25:45 -0600

    Removed unwanted files from repo history merg

--- models/mlp_model.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-
-def mlp_model(input_shape):
-    model = Sequential()
-    model.add(Dense(128, activation='relu', input_shape=(input_shape,))) # Input layer
-    model.add(Dense(64, activation='relu')) # Hidden layer
-    model.add(Dense(1, activation='sigmoid')) # Output layer
-    return model
